e67192ff96907c19259a51677e5e85878a27ae5a
Matija Čvrk <matijacvrk@gmail.com>
2016-09-03 20:20:52 +0200

    fix...

---
+++ appenv/src/app.py
@@ -0,0 +1,39 @@
+import os
+from . import models
+from .models.sessionInfo import SessionInfo
+from . import services
+from .services.sessionRepository import SessionRepository
+from flask import Flask, url_for
+from flask import jsonify
+from flask import Response
+
+
+data_root_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "data")
+
+
+_service = SessionRepository(data_storage_path=data_root_path)
+app = Flask(__name__)
+
+
+@app.route('/api', methods=['GET'])
+def api_root():
+    return 'Welcome'
+
+
+@app.route('/api/sessions', methods=['GET'])
+def api_get_sessions():
+    resp = jsonify(_service.get_sessions())
+    resp.status_code = 200
+    return resp
+
+
+@app.route('/api/sessions/<tagid>', methods=['GET'])
+def api_get_session(tagid):
+    _tag = _service.get_session(tagid)
+    resp = jsonify(_tag)
+    resp.status_code = 200
+    return resp
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
