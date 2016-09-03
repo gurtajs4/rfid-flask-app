770ab80cbfefc1d1e32ad6cbaa54f776b6ba1aea
Matija Čvrk <matijacvrk@gmail.com>
2016-09-04 01:13:09 +0200

    raw

--- appenv/src/src/static/app/config.js
+++ appenv/src/src/static/app/config.js
@@ -14,32 +14,26 @@ function config($routeProvider, $locationProvider) {
     when('/sessions', {
         templateUrl: appBaseUrl + '/sessions/session-list.html',
         controller: 'SessionsController',
-        controllerAs: 'sessions'
     }).
     when('/sessions/:id', {
         templateUrl: appBaseUrl + '/sessions/session-details.html',
-        controller: 'SessionInfoController',
-        controllerAs: 'sessionInfo'
+        controller: 'SessionInfoController'
     }).
     when('/users', {
         templateUrl: appBaseUrl + '/users/users-list.html',
-        controller: 'UsersListController',
-        controllerAs: 'users'
+        controller: 'UsersListController'
     }).
     when('/users/:id', {
         templateUrl: appBaseUrl + '/users/user-details.html',
-        controller: 'UserDetailsController',
-        controllerAs: 'userInfo'
+        controller: 'UserDetailsController'
     }).
     when('/keys', {
         templateUrl: appBaseUrl + '/keys/keys-list.html',
-        controller: 'KeysListController',
-        controllerAs: 'keys'
+        controller: 'KeysListController'
     }).
     when('/keys/:id', {
         templateUrl: appBaseUrl + '/keys/key-details.html',
-        controller: 'KeyDetailsController',
-        controllerAs: 'keyInfo'
+        controller: 'KeyDetailsController'
     }).
     otherwise({ redirectTo: '/sessions' });
 }
