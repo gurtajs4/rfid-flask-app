b7ee14e1ca7a0f445a7264c1a39a05e0eccab59c
Matija Čvrk <matijacvrk@gmail.com>
2016-09-05 23:15:43 +0200

    repasting code from previous commit on rfid-python-lib

--- rfid-python-lib/reader.py
+++ rfid-python-lib/reader.py
@@ -6,11 +6,13 @@ import signal
 from sessionRepository import SessionRepository
 from sessionInfo import SessionInfo
 
+
 continue_reading = True
 MIFAREReader = MFRC522.MFRC522()
 last_sessionId = 0
 current_userId = 0
 current_userTTL = 0
+
 
 dataStorePath = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data/tagReadings.txt")
 if not os.path.isfile(dataStorePath):
@@ -24,30 +26,18 @@ def end_read(signal, frame):
     print "Ctrl+C captured, ending read."
     MIFAREReader.GPIO_CLEEN()
 
-
 signal.signal(signal.SIGINT, end_read)
 
 print "Reader active and awaiting input..."
 
 while continue_reading:
-    if current_userTTL == time.time():
-        print "2 minutes elapsed.\nPlease register your ID card again..."
-        current_userId = -1
-        current_userTTL = -1
-        continue
     (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
     if status == MIFAREReader.MI_OK:
         print "Tag detected"
         (status, backData) = MIFAREReader.MFRC522_Anticoll()
-        if not current_userId > 0:
-            current_userTTL = time.time() + 120
-            current_userId = int(
-                str(backData[0]) + str(backData[1]) + str(backData[2]) + str(backData[3]) + str(backData[4]))
-            print "User ID: " + str(current_userId)
-        else:
+        if current_userId > 0:
             if backData != current_userId:
-                key_id = int(
-                    str(backData[0]) + str(backData[1]) + str(backData[2]) + str(backData[3]) + str(backData[4]))
+                key_id = int(str(backData[0])+str(backData[1])+str(backData[2])+str(backData[3])+str(backData[4]))
                 session = SessionInfo(session_id=last_sessionId,
                                       user_id=current_userId,
                                       time_stamp=datetime.datetime.now(),
@@ -57,3 +47,11 @@ while continue_reading:
                 current_userId = 0
                 current_userTTL = 0
                 print "Key ID: " + str(key_id)
+        else:
+            current_userTTL = time.time() + 120
+            current_userId = int(str(backData[0])+str(backData[1])+str(backData[2])+str(backData[3])+str(backData[4]))
+            print "User ID: " + str(current_userId)
+    if current_userTTL == time.time():
+        print "2 minutes elapsed.\nPlease register your ID card again..."
+        current_userId = 0
+        current_userTTL = 0
--- rfid-python-lib/sessionRepository.py
+++ rfid-python-lib/sessionRepository.py
@@ -1,4 +1,3 @@
-import os
 import json
 import datetime
 from sessionInfo import SessionInfo
@@ -29,29 +28,21 @@ class SessionRepository(object):
 
     # method for getting session from storage by sessionId
     def get_session(self, session_id):
-        if os.stat(self.data_storage_path).st_size > 0:
-            with open(self.data_storage_path, 'r') as jsonStorage:
-                sessions = json.loads(jsonStorage.read())
-                session = [value for key, value in sessions.iteritems() if value['session_id'] == session_id]
-                return session_hook_handler(session)
-        return None
+        with open(self.data_storage_path, 'r') as jsonStorage:
+            sessions = json.loads(jsonStorage)
+            session = [value for key, value in sessions.iteritems() if value['session_id'] == session_id]
+            return session_hook_handler(session)
 
     # method for getting all sessions from storage
     def get_sessions(self):
-        if os.stat(self.data_storage_path).st_size > 0:
-            with open(self.data_storage_path, 'r') as jsonStorage:
-                sessions_raw = json.loads(jsonStorage.read())
-                sessions = []
-                for key, value in sessions_raw.iteritems():
-                    sessions.append(session_hook_handler(value))
-                return sessions
-        return None
+        with open(self.data_storage_path, 'r') as jsonStorage:
+            sessions_raw = json.loads(jsonStorage)
+            sessions = []
+            for key, value in sessions_raw.iteritems():
+                sessions.append(session_hook_handler(value))
+            return sessions
 
     # method for storing a session into storage file
     def store_session(self, session):
-        sessions_raw = self.get_sessions()
-        sessions = [] if sessions_raw is None else sessions_raw
-        with open(self.data_storage_path, 'w') as jsonStorage:
-            json.dumps(sessions, jsonStorage, cls=SessionEncoder)
-            jsonStorage.flush()
-            os.fsync(jsonStorage)
+        with open(self.data_storage_path, 'a') as jsonStorage:
+            json.dumps(session, jsonStorage, cls=SessionEncoder)
