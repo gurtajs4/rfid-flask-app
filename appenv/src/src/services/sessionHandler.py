50a970b70d385de22e87800563fa23c56cc5d032
Matija Čvrk <matijacvrk@gmail.com>
2016-09-20 22:24:10 +0200

    minor adjustment with SessionHandler

--- appenv/src/src/services/sessionHandler.py
+++ appenv/src/src/services/sessionHandler.py
@@ -5,7 +5,7 @@ class SessionHandler(object):
     # method for hooking parsed json object into SessionInfo for usage in SessionRepository
     @staticmethod
     def session_hook_handler(parsed_dict):
-        return SessionInfo(session_id=parsed_dict['session_id'],
-                           user_id=parsed_dict['user_id'],
-                           time_stamp=parsed_dict['time_stamp'],
-                           key_id=parsed_dict['key_id'])
+        return SessionInfo(session_id=parsed_dict['_session_id'],
+                           user_id=parsed_dict['_user_id'],
+                           time_stamp=parsed_dict['_time_stamp'],
+                           key_id=parsed_dict['_key_id'])
