45ed408ca3c9693b34d24ac00b365a10b376ac29
Matija Čvrk <matijacvrk@gmail.com>
2016-09-20 03:48:03 +0200

    man testst - angularjs

--- appenv/src/src/static/app/sessions/session-info.controller.js
+++ appenv/src/src/static/app/sessions/session-info.controller.js
@@ -9,12 +9,16 @@ function SessionInfoController($routeParams, sessionService) {
 
     function load(){
         id = self.sessionId;
+        console.log("Session ID is " + id);
         service.session(id).then(function(session){
             self.userId = session.user_id;
             self.keyId = session.key_id;
             self.timestamp = session.time_stamp;
+            console.log("Session user ID is " + self.userId);
         });
     };
+
+    load();
 }
 
 SessionInfoController.$inject = ['$routeParams', 'sessionService'];
