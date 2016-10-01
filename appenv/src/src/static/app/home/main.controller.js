346b274b8a7a68b9c5b5e3fa2bd94205b9ed3894
Matija Čvrk <matijacvrk@gmail.com>
2016-10-01 16:20:58 +0200

    angular load minimalistic

--- appenv/src/src/static/app/home/main.controller.js
+++ appenv/src/src/static/app/home/main.controller.js
@@ -2,6 +2,7 @@ function MainController() {
     var self = this;
 
     self.tab = 0;
+    self.message = "At least AngularJS is working in main controller...";
 }
 
 angular.module('appMain').controller('MainController', MainController);
