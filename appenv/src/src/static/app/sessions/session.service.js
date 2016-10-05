f7da58d4cd0b636107c60c012aa86b2c71cc56b0
Matija Čvrk <matijacvrk@gmail.com>
2016-10-05 23:52:08 +0200

    angular model views in session.service

--- appenv/src/src/static/app/sessions/session.service.js
+++ appenv/src/src/static/app/sessions/session.service.js
@@ -8,15 +8,36 @@ function SessionService($http) {
     return service;
 
     function sessions() {
-        return $http.get('/api/sessions');
+        return $http.get('/api/sessions').then(function(response) {
+            var rawSessions = response.data;
+            var sessions = [];
+            for (i = 0; i < sessions.length) {
+                var nextSession = {
+                    sessionId: rawSessions[i]._session_id,
+                    keyId: rawSessions[i]._key_id,
+                    userId: rawSessions[i]._user_id,
+                    timestamp: rawSessions[i]._time_stamp
+                };
+                sessions.push(nextSession);
+            }
+            return sessions;
+        });
+//        return $http.get('/api/sessions');
 //        return $http.get('/api/sessions').then(function(response){
 //            return response.data;
 //        });
     };
 
-    function session(session_id) {
-        return $http.get('/api/sessions/', { params: {session_id: session_id} }).then(function(response){
-            return response.data;
+    function session(sessionId) {
+        return $http.get('/api/sessions/', { params: {session_id: sessionId} }).then(function(response){
+            var session = {
+                sessionId: response.data[i]._session_id,
+                keyId: response.data[i]._key_id,
+                userId: response.data[i]._user_id,
+                timestamp: response.data[i]._time_stamp
+            };
+            return session;
+//            return response.data;
         });
     };
 }
