6537e47ce29376fb8469dea29e052c8395a1a9b8
Matija Čvrk <matijacvrk@gmail.com>
2016-10-29 20:55:13 +0200

    managed print

--- rfid-python-lib/stop-reader.py
+++ rfid-python-lib/stop-reader.py
@@ -9,13 +9,20 @@ out, err = p.communicate()
 
 def find_process(out):
     for line in out.splitlines():
-        if 'sudo python read.py' in line or 'sudo python reader.py' in line:
-            print line.split(" ")[7]
-            print "*******************"
+        if 'PID' in line:
+            words = []
             for word in line.split(" "):
-                print word + " "
-            print "end of print"
-            pid = int(line.split(" ")[7])
+                if word is not " " and word is not "":
+                    word.push(word)
+            index = int(words.index('PID'))
+            print "Index of PID is " + index
+        if 'sudo python read.py' in line or 'sudo python reader.py' in line:
+            words = []
+            for word in line.split(" "):
+                if word is not " " and word is not "":
+                    word.push(word)
+            print "*******************"
+            pid = int(line.split(" ")[index])
             os.kill(pid, signal.SIGKILL)
 
 find_process(out)
