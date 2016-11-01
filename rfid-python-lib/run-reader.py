a4c02c181ffdb9cddd21fbfb11379a10c8bbd899
Matija Čvrk <matijacvrk@gmail.com>
2016-11-01 23:59:46 +0100

    forward slash

--- rfid-python-lib/run-reader.py
+++ rfid-python-lib/run-reader.py
@@ -3,4 +3,4 @@ import subprocess
 
 
 rel_path=os.path.dirname(os.path.realpath(__file__))
-subprocess.Popen('sudo python ' + rel_path + 'reader.py', shell=True)
+subprocess.Popen('sudo python ' + rel_path + '/reader.py', shell=True)
