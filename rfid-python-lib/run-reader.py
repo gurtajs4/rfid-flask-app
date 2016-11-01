f986890b1286f530b586f9f4058b219148a8e745
Matija Čvrk <matijacvrk@gmail.com>
2016-11-01 23:58:14 +0100

    script path

--- rfid-python-lib/run-reader.py
+++ rfid-python-lib/run-reader.py
@@ -2,5 +2,5 @@ import os
 import subprocess
 
 
-rel_path=os.path.realpath(__file__)
+rel_path=os.path.dirname(os.path.realpath(__file__))
 subprocess.Popen('sudo python ' + rel_path + 'reader.py', shell=True)
