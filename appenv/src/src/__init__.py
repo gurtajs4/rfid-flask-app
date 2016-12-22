e937865ead3b6fb38f1458280c0b4bdf3cdb85de
Matija Čvrk <matijacvrk@gmail.com>
2016-12-22 19:48:32 +0100

    error with sqlite url

--- appenv/src/src/__init__.py
+++ appenv/src/src/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-app.config.from_object('src.settings')
-
+# app.config.from_object('src.settings')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////rfid-flask-app/data/rfiddb.sqlite'
 # app.url_map.strict_slashes = False
 
 
