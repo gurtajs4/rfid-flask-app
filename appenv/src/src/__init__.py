c174ce564b8ad0288882dd5d3d656db81487b66d
Matija Čvrk <matijacvrk@gmail.com>
2016-12-23 15:40:54 +0100

    changing sqlite path

--- appenv/src/src/__init__.py
+++ appenv/src/src/__init__.py
@@ -1,8 +1,15 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 # app.config.from_object('src.settings')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////rfid-flask-app/data/rfiddb.sqlite'
+root_path = os.path.join(os.path.dirname  # /rfid-flask-app
+                        (os.path.dirname  # /rfid-flask-app/appenv
+                        (os.path.dirname  # /rfid-flask-app/appenv/src
+                        (os.path.dirname  # /rfid-flask-app/appenv/src/src
+                        (os.path.abspath(__file__))))), "data/")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + root_path + 'rfiddb.sqlite'
 # app.url_map.strict_slashes = False
 
 
