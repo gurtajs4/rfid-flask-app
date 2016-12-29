1f09f4d5db0d7feab4135092866ffefd6b8e12b2
Matija Čvrk <matijacvrk@gmail.com>
2016-12-29 04:07:33 +0100

    (AngularJS) still error on injecting to config, for safety made templateUrl in directive reader relative

--- appenv/src/src/static/app/reader/reader.directive.js
+++ appenv/src/src/static/app/reader/reader.directive.js
@@ -8,14 +8,15 @@
     reader.$inject = ['templateService'];
 
     function reader(templateService) {
-        var appBaseUrl = templateService.appBaseUrl();
+        // var appBaseUrl = templateService.appBaseUrl();
         return {
             restrict: 'EA',
             scope: {
                 tagData: "=",
                 message: "="
             },
-            templateUrl: appBaseUrl+'/reader/reader.html',
+            templateUrl: '/reader/reader.html',
+            // templateUrl: appBaseUrl+'/reader/reader.html',
             controller: 'ReaderController',
             controllerAs: 'rdc'
         }
