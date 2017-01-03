78a749eeec8cfc1cbcf66b919df882bed98ed56f
Matija Čvrk <matijacvrk@gmail.com>
2017-01-03 20:29:49 +0100

    (Flask) Getting reader data without SocketIO, trying out blocking ajax call

--- appenv/src/src/static/app/reader/reader.controller.js
+++ appenv/src/src/static/app/reader/reader.controller.js
@@ -10,41 +10,51 @@
         var self = this;
         var service = readerService;
 
-        // activate();
-        self.socketId = '';
+        activate();
 
-        function activate(socket) {
-            self.message = 'Reader will be active for the next 20 seconds...';
-            // service.initReader().then(function (response) {
-            //     console.log('Ajax response for activating reader...');
-            //     console.log(response);
-            //     // self.message = response.data.message;
-            // });
-
-            if (self.socketId === '') {
-                socket.emit('sid request');
-                socket.on('sid response', function (sid) {
-                    self.socketId = sid;
-                });
-            }
-
-            socket.emit('init reader', {room: self.socketId});
+        function activate() {
+            service.initReader().then(function (response) {
+                console.log('Ajax response for activating reader...');
+                console.log(response);
+                self.message = response.data.message;
+            });
         }
 
-        $socket().then(function (socket) {
-            console.log('creating socket instance');
+        /*
+         self.socketId = '';
 
-            activate(socket);
+         function activate(socket) {
+         self.message = 'Reader will be active for the next 20 seconds...';
+         // service.initReader().then(function (response) {
+         //     console.log('Ajax response for activating reader...');
+         //     console.log(response);
+         //     // self.message = response.data.message;
+         // });
 
-            var readerEvent = {
-                event: 'reader done',
-                room: self.socketId
-            };
-            socket.on(JSON.stringify(readerEvent), function (message) {
-                console.log('Reader is done - socket-io event from server to client');
-                console.log(message);
-                self.tagData = message['message']['data'];
-            });
-        });
+         if (self.socketId === '') {
+         socket.emit('sid request');
+         socket.on('sid response', function (sid) {
+         self.socketId = sid;
+         });
+         }
+
+         socket.emit('init reader', {room: self.socketId});
+         }
+
+         $socket().then(function (socket) {
+         console.log('creating socket instance');
+
+         activate(socket);
+
+         var readerEvent = {
+         event: 'reader done',
+         room: self.socketId
+         };
+         socket.on(JSON.stringify(readerEvent), function (message) {
+         console.log('Reader is done - socket-io event from server to client');
+         console.log(message);
+         self.tagData = message['message']['data'];
+         });
+         });*/
     }
 })();
