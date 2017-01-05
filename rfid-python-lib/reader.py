a784fc4fc1362d91b21d25ecdf3bd86f1467b3ba
Matija Čvrk <matijacvrk@gmail.com>
2017-01-05 07:03:23 +0100

    (Flask) Imported requests lib for posting data directly to the specified server route after tag reads data from reader.py

--- rfid-python-lib/reader.py
+++ rfid-python-lib/reader.py
@@ -3,6 +3,7 @@ import stat
 import time
 import datetime
 import MFRC522
+import requests
 import signal
 from sessionRepository import SessionRepository
 from sessionInfo import SessionInfo
@@ -10,13 +11,11 @@ from sessionInfo import SessionInfo
 continue_reading = True
 MIFAREReader = MFRC522.MFRC522()
 
-
 dataStorePath = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data/tagReadings.txt")
 if not os.path.isfile(dataStorePath):
     with open(dataStorePath, 'w') as dataStore:
         dataStore.write('')
     os.chmod(dataStorePath, stat.S_IRWXU | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH)
-
 
 last_storage_id = SessionRepository(data_storage_path=dataStorePath).get_last_id()
 last_sessionId = last_storage_id if last_storage_id > -1 else 0
@@ -29,6 +28,14 @@ def end_read(signal, frame):
     continue_reading = False
     print "Ctrl+C captured, ending read."
     MIFAREReader.GPIO_CLEEN()
+
+
+def post_tag_data(data):
+    url = 'http://0.0.0.0:80/api/sessions/new'
+    r = requests.post(url, data)
+    if r.status_code == requests.codes.ok or r.status_code == 200:
+        print('Tag data posted to server...')
+        print(r.text)
 
 
 signal.signal(signal.SIGINT, end_read)
@@ -56,12 +63,18 @@ while continue_reading:
                     str(backData[0]) + str(backData[1]) + str(backData[2]) + str(backData[3]) + str(backData[4]))
                 if key_id != current_userId:
                     last_sessionId += 1
-                    session = SessionInfo(session_id=last_sessionId,
-                                          user_id=current_userId,
-                                          time_stamp=datetime.datetime.now(),
-                                          key_id=key_id)
-                    sessionService = SessionRepository(data_storage_path=dataStorePath)
-                    sessionService.store_session(session)
+                    session = {
+                        'user_id': str(current_userId),
+                        'key_id': str(key_id),
+                        'timestamp': datetime.datetime.now()
+                    }
+                    post_tag_data(session)
+                    # session = SessionInfo(session_id=last_sessionId,
+                    #                       user_id=current_userId,
+                    #                       time_stamp=datetime.datetime.now(),
+                    #                       key_id=key_id)
+                    # sessionService = SessionRepository(data_storage_path=dataStorePath)
+                    # sessionService.store_session(session)
                     current_userId = -1
                     current_userTTL = -1
                     print "Key ID: " + str(key_id)
