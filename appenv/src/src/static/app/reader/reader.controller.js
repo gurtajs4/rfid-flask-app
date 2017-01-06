1bda7281b13bbd62defebcbbe93335109a9ba802
Matija Čvrk <matijacvrk@gmail.com>
2017-01-06 17:26:05 +0100

    (AngularJS) timeout is better than apply - because apply not good from within another apply call

--- appenv/src/src/static/app/reader/reader.controller.js
+++ appenv/src/src/static/app/reader/reader.controller.js
@@ -5,8 +5,8 @@
         .module('appMain')
         .controller('ReaderController', ReaderController);
 
-    ReaderController.$inject = ['$scope', 'readerService'];
-    function ReaderController($scope, readerService) {
+    ReaderController.$inject = ['$scope', '$timeout', 'readerService'];
+    function ReaderController($scope, $timeout, readerService) {
         var service = readerService;
 
         activate();
@@ -14,10 +14,10 @@
         function activate() {
             service.initReader().then(function (response) {
                 console.log('Ajax response for activating reader...');
-                $scope.$apply(function () {
+                $timeout(function () {
                     $scope.tagData = response.data['data'];
                     $scope.message = response.data['message'];
-                });
+                }, 0);
             });
         }
     }
