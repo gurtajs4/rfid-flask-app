74ec304dfdef95143da0edd04c0633cb5de6ccd0
Matija Čvrk <matijacvrk@gmail.com>
2017-01-07 02:30:54 +0100

    (AngularJS) More console.log() statments - going deeper

--- appenv/src/src/static/app/reader/reader.controller.js
+++ appenv/src/src/static/app/reader/reader.controller.js
@@ -17,13 +17,24 @@
             service.initReader().then(function (response) {
                 console.log('Ajax response for activating reader...');
                 console.log(response.data);
+                console.log(response.data.data);
                 console.log(response.data.message);
                 console.log($scope.tagData);
                 console.log($scope.message);
+                var rData = response.data.data;
+                var rMessage = response.data.message;
+                console.log(rData);
+                console.log(rMessage);
                 setTimeout(function () {
                     $scope.$apply(function () {
-                        $scope.tagData = response.data['data'];
-                        $scope.message = response.data['message'];
+                        $scope.tagData = response.data.data;
+                        $scope.message = response.data.message;
+                        console.log($scope.tagData);
+                        console.log($scope.message);
+                        $scope.tagData = rData;
+                        $scope.message = rMessage;
+                        console.log($scope.tagData);
+                        console.log($scope.message);
                     });
                     console.log($scope.tagData);
                     console.log($scope.message);
