79c4d7f88e30667906b231313f02057d88cfe9be
Matija Čvrk <matijacvrk@gmail.com>
2017-01-14 02:25:33 +0100

    (Reader) Moved MFRC dependancies back to lib folder

--- passive_reader/run-reader.py
+++ passive_reader/run-reader.py
@@ -1,7 +1,10 @@
 import os
 import subprocess
 
-
-rel_path=os.path.dirname(os.path.realpath(__file__))
-subprocess.Popen('sudo python ' + rel_path + '/bkc_user_reader.py', shell=True)
+dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+process_name = 'sudo python ' + dir_path + '\\rfid_python_lib\\bkc_user_reader.py'
+print(process_name)
+subprocess.Popen(process_name, shell=True)
+# rel_path=os.path.dirname(os.path.realpath(__file__))
+# subprocess.Popen('sudo python ' + rel_path + '/bkc_user_reader.py', shell=True)
 # subprocess.Popen('sudo python ' + rel_path + '/background_reader.py', shell=True)
