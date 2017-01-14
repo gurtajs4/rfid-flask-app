31f3b03e5c45af9c46dad639b9ce4c65e84e7bca
Matija Čvrk <matijacvrk@gmail.com>
2017-01-14 02:27:38 +0100

    (Reader) Forward slashes on popen

--- passive_reader/run-reader.py
+++ passive_reader/run-reader.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-process_name = 'sudo python ' + dir_path + '\\rfid_python_lib\\bkc_user_reader.py'
+process_name = 'sudo python ' + dir_path + '/rfid_python_lib/bkc_user_reader.py'
 print(process_name)
 subprocess.Popen(process_name, shell=True)
 # rel_path=os.path.dirname(os.path.realpath(__file__))
