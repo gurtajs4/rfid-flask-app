f859a23d76a11bab2f9d37066eb5036877921919
Matija Čvrk <matijacvrk@gmail.com>
2017-01-23 13:09:11 +0100

    (Flask) messed up images path in config

--- appenv/src/src/config.py
+++ appenv/src/src/config.py
@@ -3,6 +3,6 @@ import os
 sqlite_dir_path = os.path.join(os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))), 'data')
 DATABASE_URI = os.path.join(sqlite_dir_path, 'rfid-db.sqlite')
-UPLOAD_URI = os.path.join(os.path.abspath(__file__), 'static/images')
+UPLOAD_URI = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/images')
 
 SECRET_KEY = 'the_secret_key'
