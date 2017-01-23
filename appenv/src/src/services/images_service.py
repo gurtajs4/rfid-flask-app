51df4fd79357953c178f85abc40e283f9d0ae8a8
Matija Čvrk <matijacvrk@gmail.com>
2017-01-23 12:56:14 +0100

    (Flask) Typing out dir() of image file

--- appenv/src/src/services/images_service.py
+++ appenv/src/src/services/images_service.py
@@ -36,6 +36,7 @@ def save_img(image):
     if None is image:
         return os.path.join(upu, 'default.png'), 1
     print('From server - image service - image file received %s' % image)
+    print('From server - image service - image file has %s' % dir(image))
     if '' == image['name'] and '' == image['filename']:
         return os.path.join(upu, 'default.png'), 1
     file = image
