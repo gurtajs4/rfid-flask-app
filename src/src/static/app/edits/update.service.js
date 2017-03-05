408ad520da27bd4d3140a4964af531b5927733d3
Matija Čvrk <matijacvrk@gmail.com>
2017-03-05 03:31:03 +0100

    (AngularJS) update service - get functions

--- src/src/static/app/edits/update.service.js
+++ src/src/static/app/edits/update.service.js
@@ -5,8 +5,8 @@
     updateService.$inject = ['$http', 'keysService', 'usersService', 'Upload'];
     function updateService($http, keysService, usersService, Upload) {
         var service = {
-            getKey: keysService.getKey,
-            getUser: usersService.getUser,
+            getKey: keysService.getKey(),
+            getUser: usersService.getUser(),
             keyUpdate: keyUpdate,
             userUpdate: userUpdate
         };
