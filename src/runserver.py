283764335018d086fb67c064611ddc2a4577873b
Matija Čvrk <matijacvrk@gmail.com>
2017-05-08 14:16:39 +0200

    (cleanup2) runserver script without socketio

--- src/runserver.py
+++ src/runserver.py
@@ -1,7 +1,4 @@
 from src import app
-from src import socket_io
-
 
 if __name__ == '__main__':
-    socket_io.run(app=app, host='0.0.0.0', port=80, debug=True)
-    # app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
