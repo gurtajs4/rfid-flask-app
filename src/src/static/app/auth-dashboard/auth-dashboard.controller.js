548b51014da7121737f99ea5f6ba2c05e05ad13d
Matija Čvrk <matijacvrk@gmail.com>
2017-07-02 12:12:59 +0200

    (AngularJS) account dashboard now refreshes username on user login data changed - checking via authService

--- src/src/static/app/auth-dashboard/auth-dashboard.controller.js
+++ src/src/static/app/auth-dashboard/auth-dashboard.controller.js
@@ -5,14 +5,46 @@
         .module('appMain')
         .controller('AuthDashboardController', AuthDashboardController);
 
-    AuthDashboardController.$inject = ['$scope', '$location', 'authService'];
-    function AuthDashboardController($scope, $location, authService) {
-        var service = authService;
-
+    AuthDashboardController.$inject = ['$scope', '$location', 'authService', 'userService'];
+    function AuthDashboardController($scope, $location, authService, userService) {
         $scope.logout = logout;
+        $scope.editProfile = editProfile;
 
-        function logout(callback) {
-            service.clearCredentials(callback);
+        init();
+
+        $scope.$on('$routeChangeStart', function (previous, next) {
+            var currentServiceUsername = authService.getCredentials().username;
+            if ($scope.username != currentServiceUsername) {
+                setUsername(currentServiceUsername);
+            }
+        });
+
+        function init() {
+            setUsername(authService.getCredentials().username);
+        }
+
+        function setUsername(username) {
+            $scope.username = username;
+        }
+
+        function editProfile() {
+            userService.search($scope.username)
+                .then(function (response) {
+                    var userId = null;
+                    angular.forEach(response.data, function (value, key) {
+                        var user = JSON.parse(value);
+                        if (user) {
+                            userId = user.id;
+                        }
+                    });
+                    if (userId) {
+                        $location.url('/users/edit/' + userId.toString());
+                    }
+                });
+        }
+
+        function logout() {
+            authService.clearCredentials();
             $location.url('/home');
         }
     }
