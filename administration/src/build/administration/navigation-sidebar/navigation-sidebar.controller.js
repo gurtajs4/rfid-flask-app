0c7b4d302265da764184ae7fb679796d200e544a
Matija Čvrk <matijacvrk@gmail.com>
2017-09-04 04:21:42 +0200

    (AngularJS:sidebar) sidebar with hiding links

--- administration/src/build/administration/navigation-sidebar/navigation-sidebar.controller.js
+++ administration/src/build/administration/navigation-sidebar/navigation-sidebar.controller.js
@@ -5,15 +5,13 @@
         .module('administration')
         .controller('SidebarController', SidebarController);
 
-    SidebarController.$inject = ['$scope', '$log'];
-    function SidebarController($scope, $log) {
-        $scope.common = {label: 'Opcije brze pretrage'};
+    SidebarController.$inject = ['$scope'];
+    function SidebarController($scope) {
+        $scope.basic = {label: 'Opcije brze pretrage'};
         $scope.extended = {label: 'Pregled svih podataka'};
         $scope.settings = {label: 'Opcije za pohranu'};
 
-        /* {id: 0, route: '/', name: 'Naslovnica'}*/
-
-        $scope.commonRoutes = [
+        $scope.basicRoutes = [
             {id: 7, route: '/users/search', name: 'Pretraga korisnika'},
             {id: 2, route: '/keys/search', name: 'Pretraga ključeva'}
         ];
@@ -26,18 +24,5 @@
             {id: 4, route: '/keys/register', name: 'Registracija ključa'},
             {id: 3, route: '/keys/seed', name: 'Uvoz ključeva'}
         ];
-
-        /*
-         function displayOptions() {
-         if ($scope.common.showOptions) {
-         // getCommonOptions
-         }
-         if ($scope.extended.showOptions) {
-         // getCommonOptions
-         }
-         if ($scope.settings.showOptions) {
-         // getCommonOptions
-         }
-         }*/
     }
 })();
